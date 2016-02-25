b7159296192d8043d4cb6f84775f042fb011c01e
Romaric <romaric.juniet@gmail.com>
2016-02-25 21:51:53 +0800

    First commit

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
+/*
+ * Just a HTTP server receiving POST request on /go
+ */
 'use strict';
 
-var randomSentences = require('./random-sentences');
-var express = require('express');
-var app = express();
-
-var bodyParser = require('body-parser');
-app.use(bodyParser.text({limit: '5mb'}));
-app.use(bodyParser.urlencoded({limit: '5mb', extended: true}));
+// Imports: Core logic, express and bodyParser
+const randomSentences = require('./random-sentences');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.text({ limit: '5mb' }));
            
+/*
+ * /go endpoint
+ * POST /go[?order=1]
+ * The body of the request is the text used for generation. 
+ * The body should have at least 2 sentences. Dot '.' is used to recognise begining of sentences.
+ * Order is the order of n-grams to use. Optionnal. High is more reliable (more sense), lower is more fun
+ */
 app.post('/go', function(req, res){
-  var text = String(req.body);
+  const text = String(req.body);
   const order = parseInt(req.query.order) || 1;
 
   if (!text || text == '' || text.indexOf('.') == -1)
-    return res.status(422).send('No sentence');
+    return res.status(422).send('At least 2 sentences needed to generate text');
 
-  var data = randomSentences.main(text, order);
-
+  // Get some random text an return it
+  const data = randomSentences.main(text, order);
   res.setHeader('Access-Control-Allow-Origin', '*');
-  
   return res.status(200).send(data);
 });
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Example app listening on port ' + port);
 })
--- random-sentences.js
+++ random-sentences.js
@@ -1,15 +1,30 @@
+/*
+ * Generate 10 random sentences from an input text.
+ * Build a graph of n-grams and their successors
+ */
 'use strict';
 
 String.prototype.capitalize = function () {
   return this.length > 0 ? this[0].toUpperCase() + this.slice(1) : '';
 }
 
+// Node of the graph. Representes one n-gram.
+// Edges are maps of each 1-gram successor of the n-gram
 class Node {
   constructor (nGram) {
+    // N-gram of the node.
     this.nGram = nGram;
+
+    // Edges of the n-gram: Each successors of the n-gram of the node
+    // Map with key=1-gram successor of the n-gram of the node
+    //          value=Number of occurences of this 1-gram after the n-gram of the node
     this.edges = new Map();
+
+    // Number of occurences of the N-grams. Equals the sum of each value of this.edges
     this.occurrences = 0;
   }
+
+  // Add an edge (a 1-gram) to the node
   addEdge(nGram)  {
     if (!this.edges.has(nGram)) {
       this.edges.set(nGram, 0);
@@ -17,34 +32,55 @@ class Node {
     this.edges.set(nGram, this.edges.get(nGram)+1);
     this.occurrences++;
   }
+
+  // Get a random next 1-gram, respecting to the frequencies
   generateNext() {
-    const number = Math.floor(this.occurrences*Math.random()); 
-    let i = 0;
+    // Random number between 0 and the number of occurences
+    const randomNumber = Math.floor(this.occurrences*Math.random()); 
+
+    // The randomly picked node
     let node = null;
+
+    // The array of nodes to pick from
     const nodesArray = [...this.edges];
-    for (var j = 0 ; i <= number && j < nodesArray.length ; j++) {
-      node = nodesArray[j]; 
-      i += node[1];
+
+    // Loop on the array, adding the number of occurences of each node, until reaching the random number.
+    for (let iRandomNumber = 0, iArray = 0; iRandomNumber <= randomNumber && iArray < nodesArray.length ; iArray++) {
+      // Get the node pointed by the array iterator
+      node = nodesArray[iArray]; 
+
+      // Increase the pointer of the random number by the number of occurences of this node
+      iRandomNumber += node[1];
     };
+
+    // Return the 1-gram of the randomly picked node
     return node[0];
   }
 }
 
-// Split a string into word-level 1-grams
+// Split a string into word-level 1-grams. Lots to improve here.
 function splitInWords(text) {
   return text.replace(/[^a-zA-Z0-9. ]/g, ' ')
-//  .replace(/([^A-Z])\./g, '$1 .')
-  .replace(/\s+/g,' ')
-  .trim()
-  .toLowerCase()
-  .split(' ');
+    .replace(/\s+/g,' ')
+    .trim()
+    .toLowerCase()
+    .split(' ');
 }
 
-
+/*
+ * Generate 10 random sentences from an input text.
+ * Build a graph of n-grams and their successors
+ * Text = input text to generate from
+ * Order = order of the n-gram
+ */
 var main = function(text, order) {
-  const words = splitInWords(text)
-  const sentences = [];
+  // Split the input text in words
+  const words = splitInWords(text);
 
+  // Generated sentences
+  let sentences = [];
+
+  // Build the graph. The grap is a map of nodes. A node is identified by its n-gram
   let nodes = new Map();
   for(let i = 0; i <= words.length - order; i++) {
     const nGram = words.slice(i, i+order).join(' ');
@@ -55,20 +91,37 @@ var main = function(text, order) {
     nodes.get(nGram).addEdge(words[i + order] || null);
   }
 
+  // Sort the n-grams by occurences
   const sortedNodes = new Map([...nodes].sort((a,b) => a[1].occurrences > b[1].occurrences ? 1 : a[1].occurrences < b[1].occurrences ? -1 : 0).reverse());
 
   // TODO: Select begining according to probabilities
+  // Generate 10 random sentences
   for (let i in Array.from(Array(10).keys())) {
+    // Get all the n-grams that end sentences
     const endSentences = [...sortedNodes.keys()].filter(key => key.endsWith('.'));
+
+    // Pick a random ending n-gram
     const randomBeginingKey = endSentences[Math.floor(endSentences.length*Math.random())];
+
+    // Generate the sentence
     if (sortedNodes.get(randomBeginingKey)) {
+      // Generate the first 1-gram
       let generatedWord = sortedNodes.get(randomBeginingKey).generateNext();
+
+      // Generated sentence
       let sentence = splitInWords(randomBeginingKey);
+
+      // Continue the sentence until we reach an end of sentence (dot)
       while (generatedWord && !generatedWord.endsWith(".")) {
-        const generatedNode = sortedNodes.get(sentence.slice(-order).join(' '))
+        // Build the n-gram
+        const generatedNode = sortedNodes.get(sentence.slice(-order).join(' '));
+
+        // Add the new generated node. If there is no node, we reached the end of the sentence
         generatedWord = generatedNode ? generatedNode.generateNext() : ".";
         sentence.push(generatedWord);
       }
+
+      // Push our new sentence to the returned array
       sentences.push(sentence.slice(order).join(' ').capitalize());
     }
   }
